50fe85fde461c8cb426d6224d58f16abddb6503f
Reflex8 <108949608+Reflex8@users.noreply.github.com>
2023-08-05 22:26:42 -0400

    Add files via upload
    Rithik's TabBar - temp version, waiting for Ayush's updated version

---
+++ TabBar/TabBar/TabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarApp.swift
+//  TabBar
+//
+//  Created by Rajeev Kumar on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct TabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
