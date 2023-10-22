7f736a049d30cc21c4077ef53120ca42d537c97a
Reflex8 <108949608+Reflex8@users.noreply.github.com>
2023-10-22 18:17:34 -0400

    Add files via upload
    - [ ] Changed
            - [ ] ContentView
            - [ ] ViewModel
            - [ ] ColorSwitching
            - [ ] AddQuoteView/EditQuoteView
            - [ ] TabBar
            - [ ] TagView

---
+++ QuoteBook - GOOGLE AUTH/QuoteBook/QuoteBookApp.swift
@@ -0,0 +1,28 @@
+//
+//  QuoteBookApp.swift
+//  QuoteBook
+//
+//  Created by Rajeev Kumar on 6/30/23.
+//
+import Foundation
+import SwiftUI
+import Firebase
+
+@main
+struct QuoteBookApp: App {
+    @AppStorage("signIn") var isSignIn = false
+
+    
+    init(){
+        FirebaseApp.configure()
+    }
+    var body: some Scene {
+        WindowGroup {
+            if !isSignIn {
+                LoginScreen()
+            } else {
+                ContentView()
+            }
+        }
+    }
+}
